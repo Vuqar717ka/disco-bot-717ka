4873987f0042f0b906a92fa560e296e011da44b3
Vuqar717ka <42815314+Vuqar717ka@users.noreply.github.com>
2018-09-10 15:22:32 +0400

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   let guild = member.guild;
-  member.send('niye gittin?');
-  guild.defaultChannel.sendMessage(`${member.user.username} gitti.`);
+  member.send('Seni Özleyeceğiz!');
+  guild.defaultChannel.send(`bb`);
 };
