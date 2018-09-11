d09e691c617e4b67270725ffab025403b3b96472
Vuqar717ka <42815314+Vuqar717ka@users.noreply.github.com>
2018-09-11 16:20:48 +0400

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,38 +1,95 @@
-const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
+const { stripIndents, oneLine } = require('common-tags');
+const Command = require('../base');
+const disambiguation = require('../../util').disambiguation;
 
-var prefix = ayarlar.prefix;
+module.exports = class HelpCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'yardım',
+			group: 'util',
+			memberName: 'yardım',
+			aliases: ['commands', 'y', 'komutlar', 'help', 'halp', 'h'],
+			description: 'Tüm komutları listeler. İsterseniz bir komut hakkında yardım eder.',
+			details: oneLine`
+					Yardım için herhangi bir komut adı belirtebilirsiniz.
+					Bir komut belirtilmezse, mevcut tüm kullanılabilir komutlar listelenir.
+			`,
+			examples: ['yardım hepsi', 'yardım <komut>'],
+			
+			args: [
+				{
+					key: 'command',
+					prompt: 'Hangi komut hakkında yardım istiyorsun?',
+					type: 'string',
+					default: 'hepsi'
+				}
+			]
+		});
+	}
 
-exports.run = (client, message, params) => {
-  if (!params[0]) {
-    const commandNames = Array.from(client.commands.keys());
-    const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 20);
-    message.author.sendCode('asciidoc', `= Komut Listesi =\n\n[Komut hakkında bilgi için ${ayarlar.prefix}yardım <komut adı>]\n\n${client.commands.map(c => `${ayarlar.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`);
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
-  } else {
-    let command = params[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
-      message.author.sendCode('asciidoc', `= ${command.help.name} = \n${command.help.description}\nDoğru kullanım: ` + prefix + `${command.help.usage}`);
-    }
-  }
-};
+	async run(msg, args) {
+		const groups = this.client.registry.groups;
+		const commands = this.client.registry.findCommands(args.command, false, msg);
+		const showAll = args.command && args.command.toLowerCase() === 'hepsi';
+		if(args.command && !showAll) {
+			if(commands.length === 1) {
+				let help = stripIndents`
+					${oneLine`
+						__Command **${commands[0].name}**:__ ${commands[0].description}
+						${commands[0].guildOnly ? ' (Sadece sunucularda kullanılabilir)' : ''}
+					`}
+					**Format:** ${msg.anyUsage(`${commands[0].name}${commands[0].format ? ` ${commands[0].format}` : ''}`)}
+				`;
+				if(commands[0].aliases.length > 0) help += `\n**Kısaltmalar:** ${commands[0].aliases.join(', ')}`;
+				help += `\n${oneLine`
+					**Group:** ${commands[0].group.name}
+					(\`${commands[0].groupID}:${commands[0].memberName}\`)
+				`}`;
+				if(commands[0].details) help += `\n**Detaylar:** ${commands[0].details}`;
+				if(commands[0].examples) help += `\n**Örnekler:**\n${commands[0].examples.join('\n')}`;
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['h', 'halp', 'help', 'y'],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'yardım',
-  description: 'Tüm komutları gösterir.',
-  usage: 'yardım [komut]'
+				const messages = [];
+				try {
+					messages.push(await msg.direct(help));
+					if(msg.channel.type !== 'dm') messages.push(await msg.reply('Özel mesajlarını kontrol et. :postbox:'));
+				} catch(err) {
+					messages.push(await msg.reply('Komutları özel mesaj olarak sana gönderemiyorum. Sanırım özel mesajların kapalı.'));
+				}
+				return messages;
+			} else if(commands.length > 1) {
+				return msg.reply(disambiguation(commands, 'commands'));
+			} else {
+				return msg.reply(
+					`Geçersiz komut. ${msg.usage(
+						null, msg.channel.type === 'dm' ? null : undefined, msg.channel.type === 'dm' ? null : undefined
+					)} komutunu kullanarak komut listesini görebilirsiniz.`
+				);
+			}
+		} else {
+			const messages = [];
+			try {
+				messages.push(await msg.direct(stripIndents`
+					${oneLine`
+						${msg.guild || 'Sunucu ismi bulunamadı!'} sunucusunda komut kullanmak için aşağıdaki örneği inceleyin.
+						Örnek: ${Command.usage('komut', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
+						Örnek: ${Command.usage('prefix', msg.guild ? msg.guild.commandPrefix : null, this.client.user)}.
+					`}
+					Özel mesajda komut kullanırken, ön-ek (prefix) kullanmanıza gerek yok! Örnek: ${Command.usage('komut', null, null)}
+					__**${showAll ? 'Tüm komutlar' : `${msg.guild + ' sunucusunda' || 'bu Özel Mesaj içinde'} kullanılabilir komutlar:`}**__
+					${(showAll ? groups : groups.filter(grp => grp.commands.some(cmd => cmd.isUsable(msg))))
+						.map(grp => stripIndents`
+							__${grp.name}__
+							${(showAll ? grp.commands : grp.commands.filter(cmd => cmd.isUsable(msg)))
+								.map(cmd => `**${cmd.name}:** ${cmd.description}`).join('\n')
+							}
+						`).join('\n\n')
+					}
+				`, { split: true }));
+				if(msg.channel.type !== 'dm') messages.push(await msg.reply('Özel mesajlarını kontrol et. :postbox:'));
+			} catch(err) {
+				messages.push(await msg.reply('Komutları özel mesaj olarak sana gönderemiyorum. Sanırım özel mesajların kapalı.'));
+			}
+			return messages;
+		}
+	}
 };
